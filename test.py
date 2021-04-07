0b55c23a3dd486c4c5180959a0378a0e8e88ef06
Li <leochen1990@gmail.com>
2021-04-07 12:00:35 +0200

    system specific environment

--- test.py
+++ test.py
@@ -11,37 +11,64 @@ else:
 submodules = [folder for folder in os.listdir(".") if os.path.isdir(folder) and ".git" in os.listdir(folder)]
 print("Found submodules:", submodules)
 
-shell_script = [' ']
 
 if not os.path.exists("temp"):
     os.mkdir("temp")
 
-for module in submodules:
-    # get commit id of sub module
-    label = subprocess.check_output(["git", "ls-files", "-s", module]).decode()
-    commit = label.split(" ")[1]
-    
-    if commit == TESTED.get(module):
-        print("Skipping tested module: {} {}".format(module, commit))
-        continue
 
-    print("Generate test for {} {}".format(module, commit))
+def generate_test(shell_script, os_type):
+    for module in submodules:
+        # get commit id of sub module
+        label = subprocess.check_output(["git", "ls-files", "-s", module]).decode()
+        commit = label.split(" ")[1]
+        
+        env_file_cross_platform = os.path.join(module, "environment.yml")
+        if os.path.exists(env_file_cross_platform):
+            env_file = env_file_cross_platform
+        else:
+            env_file = os.path.join(module, "environment.{}.yml".fromat(os_type))
 
-    shell_script.extend([
-        "conda env create -n {} -f {}/environment.yml".format(module, module),
-        "conda activate {}".format(module),
-        "cd {}".format(module),
-        "python test.py",
-        "echo {} has passed test!".format(module),
-        "cd ../"
-    ])
+        if not os.path.exists(env_file):
+            print("Skipping os: {}".format(os_type))
+            continue
+        
+        if commit == TESTED.get(module):
+            print("Skipping tested module: {} {}".format(module, commit))
+            continue
+
+        print("Generate test for {} {}".format(module, commit))
+
+        shell_script.extend([
+            "conda env create -n {} -f {}".format(module, env_file),
+            "conda activate {}".format(module),
+            "cd {}".format(module),
+            "python test.py",
+            "echo {} has passed test!".format(module),
+            "cd ../"
+        ])
 
 
-f = open("temp/test.bat", "w")
-f.write(os.linesep.join(shell_script))
+
+
+shell_script_windows = ['echo start testing']
+shell_script_macos = ['echo start testing']
+shell_script_linux = ['echo start testing']
+
+generate_test(shell_script_windows, 'windows')
+generate_test(shell_script_macos, 'macos')
+generate_test(shell_script_linux, 'linux')
+
+
+f = open("temp/test.windows.bat", "w")
+f.write(os.linesep.join(shell_script_windows))
 f.close()
 
-shell_script.insert(0, "eval \"$(conda shell.bash hook)\"")
-f = open("temp/test.sh", "w")
-f.write(os.linesep.join(shell_script))
+shell_script_macos.insert(0, "eval \"$(conda shell.bash hook)\"")
+f = open("temp/test.macos.sh", "w")
+f.write(os.linesep.join(shell_script_macos))
+f.close()
+
+shell_script_linux.insert(0, "eval \"$(conda shell.bash hook)\"")
+f = open("temp/test.linux.sh", "w")
+f.write(os.linesep.join(shell_script_linux))
 f.close()
